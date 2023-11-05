a284c3d6c55515eee3d9c7c25d8c62b045678acc
Teodoro Avendaño <teodoroavendano7@gmail.com>
2023-11-04 19:38:33 -0500

    feat: retornos de una función

---
+++ es6.js
@@ -0,0 +1,26 @@
+// template literals
+
+    function inicio(){
+        function saludar(nombre){
+            return "Hola" + nombre;
+            }
+        function calcularNacimiento(edad){
+            return 2023 - edad;
+        }
+        let nombrePeersona = prompt("Ingresa tu nombre");
+        let resultadoSaludo = saludar(nombrePersona);
+        console.log(nombrePersona); 
+    
+        let edadPersona = prompt("Ingresa tu edad");
+        let resultadoCalculo = calcularNacimiento(edadPersona);
+        console.log(resultadoSaludo + ("tu año de nacimiento es ") + resultadoCalculo);
+    }
+    inicio();
+
+
+
+
+
+
+
+
